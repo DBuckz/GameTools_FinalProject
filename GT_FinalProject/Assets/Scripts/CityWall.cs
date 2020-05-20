e8dc0e4d2e3b2bc2c0fb42ca228894e130d34693
DBuckz <37370298+DBuckz@users.noreply.github.com>
2020-05-20 16:54:38 +0100

    Polishing city generation
    - City blocks no longer clip with each other
    - Outer city walls added

---
+++ GT_FinalProject/Assets/Scripts/CityWall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityWall : MonoBehaviour
+{
+    public GameObject WallPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int y = 0; y < 10; y += 2)
+        {
+            for (int i = -25; i < 26; i += 50)
+            {
+                for (int j = -24; j < 26; j += 2)
+                {
+                    Instantiate(WallPrefab, new Vector3(i, y, j), Quaternion.identity);
+                }
+                for (int j = -24; j < 26; j += 2)
+                {
+                    Instantiate(WallPrefab, new Vector3(j, y, i), Quaternion.identity);
+                }
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
