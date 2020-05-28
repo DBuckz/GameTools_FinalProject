c3d7a7cdf8454d81d7ef98058a79d3ccbb780473
DBuckz <37370298+DBuckz@users.noreply.github.com>
2020-05-28 15:14:59 +0100

    Updated player movement
    -Swapped out Rigidbody based movement for character controller

---
+++ GT_FinalProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 3f;
+    public float jumpSpeed = 3f;
+    public float G = -9.81f;
+    public float groundDist = 0.2f;
+
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpSpeed * -2f * G);
+        }
+
+        velocity.y += G * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
